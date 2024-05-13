6ac3c9db49c8fe733c45aae33d2adf62de03ccba
Nabil <nabiladnan3007@gmail.com>
2024-05-13 16:05:16 +0600

    starting my C#

---
+++ First/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+using System.Threading.Channels;
+class First
+{
+    public static void Main(string[] args)
+    {
+
+        // const string name ="apple iphone"; 
+        // double Price = 150000;
+        // int sold = 7;
+        // bool is_available = true;
+
+        // Console.WriteLine(name);
+        // Console.WriteLine(Price);
+        // Console.WriteLine(is_available);
+        // Console.WriteLine(sold);
+
+        // int ans = 104;
+        // char a = (char)ans;
+
+        // Console.WriteLine(Convert.ToString(ans));
+
+        // string? student_name;
+        // int age;
+        // double due_money;
+        // bool is_registered;
+
+        // student_name = Console.ReadLine(); // handle nullablity
+        // age = Convert.ToInt32(Console.ReadLine());
+        // due_money = Convert.ToDouble(Console.ReadLine());
+        // is_registered = Convert.ToBoolean(Console.ReadLine());
+
+        // Console.Write("Hello " + student_name + ". ");
+        // Console.Write("You are " + age + " years old.");
+        // Console.Write("Your Due in cafeteria is " + due_money + " bdt");
+
+
+        // int number1 = 25, number2 = 25;
+        // int result = number1 + number2;
+        // Console.WriteLine($"{number1} + {number2} = {result}"); // string interpolation to format my o/p
+        // Console.WriteLine("{0} + {1} = {2}", number1, number2, result);
+
+        // int basee, height;
+        // Console.Write("Base = ");
+        // basee = Convert.ToInt32(Console.ReadLine());
+        // height = Convert.ToInt32(Console.ReadLine());
+        // double ans = 0.5 * basee * height;
+        // Console.WriteLine($"Area = {ans}");
+
+
+
+        // string input;
+        // Console.WriteLine("enter input : ");
+        // input = Console.ReadLine();
+        // // Console.WriteLine($"{input} is a day");
+
+        // switch(input)
+        // {
+        //     case "friday":
+        //     case "saturday":
+        //         Console.WriteLine($"{input} is a off day");
+        //         break;
+        //     default:
+        //         Console.WriteLine($"{input} is a weekday");
+        //         break;    
+        // }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+    }
+}
---
+++ methods/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+class method_ops
+{
+
+    public static int Add(int n, int m)
+    {
+        return n + m;
+    }
+
+    public static void message(string t)
+    {
+        Console.WriteLine(t);
+    }
+    public static void Main(string[] args)
+    {
+        message("hello");
+        int result = Add(10, 20);
+        Console.WriteLine($"ans is = {result}");
+        message("goodbye");
+
+    }
+}
