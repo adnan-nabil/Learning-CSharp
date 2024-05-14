1d9c6345083ad6b45bda3301c6a43109dd1a213f
Nabil <nabiladnan3007@gmail.com>
2024-05-14 09:17:02 +0600

    class object creation

---
+++ basic class -object/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+
+class Person
+{
+    public string? name;
+    public int age;
+}
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        Person p1 = new();
+        Person p2 = new();
+    
+
+        p1.name = "nabil";
+        p2.name = "nahid";
+        p1.age = 26;
+        p2.age = 22;
+
+        System.Console.WriteLine($"name of p1 = {p1.name}");
+    }
+}
