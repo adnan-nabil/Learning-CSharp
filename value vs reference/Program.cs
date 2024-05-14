4eafd12ba9b73e4f62e49b9729a65db69955d87b
Nabil <nabiladnan3007@gmail.com>
2024-05-14 16:40:44 +0600

    reference typevs value type

---
+++ value vs reference/Program.cs
@@ -0,0 +1,34 @@
+﻿class Person
+{
+    public int age;
+    public string? name;
+
+    public Person(string s, int n)
+    {
+        name = s;
+        age = n;
+    }
+
+    public void display()
+    {
+        System.Console.WriteLine($"name = {name} ; age = {age}");
+    }
+}
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        Person p1 = new("nabil", 26);
+        p1.display();
+
+        Person p2 = p1;
+        p2.display();
+
+        p2.name = "nahid";
+        p2.age = 22;
+
+        p1.display();
+        p2.display();
+    }
+}
